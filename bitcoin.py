89e6adcd217ba845c7b3b00174a9c6d584c5e16f
Jum <jun197705@yahoo.co.jp>
2022-08-01 01:49:12 +0900

    Update second

--- bitcoin.py
+++ bitcoin.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
+# In[6]:
 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 import requests
-import chromedriver_binary
 from time import sleep
 browser = webdriver.Chrome()
 browser.get('https://coinmarketcap.com/ja/currencies/bitcoin/')
@@ -16,13 +15,13 @@ coin_price=browser.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/di
 #ニュース一覧を上から順に繰り返します/body/div[1]/div/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div/span")
 
 
-# In[37]:
+# In[7]:
 
 
 coin_text="ビットコイン(BTC) "
 
 
-# In[38]:
+# In[8]:
 
 
 coin_text=coin_text+coin_price.text
@@ -42,6 +41,12 @@ payload = {
 res = requests.post( WEBHOOK_URL, json=payload )
 print( res.status_code )
 print( json.dumps( json.loads(res.content), indent=4, ensure_ascii=False ) )
+
+
+# In[41]:
+
+
+get_ipython().run_line_magic('run', 'C:\\Users\\jun19\\Downloads/bitcoin.py')
 
 
 # In[ ]:
