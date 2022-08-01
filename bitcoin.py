bfbb72dd50c65f2179441337804d55f8176ed65c
Jum <jun197705@yahoo.co.jp>
2022-08-02 01:31:20 +0900

    Add

--- bitcoin.py
+++ bitcoin.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[11]:
+
+
+get_ipython().system('pip install selenium')
+
+
+# In[12]:
+
+
+get_ipython().system('pip install chromedriver-binary==103.0.5060.134')
+
+
+# In[13]:
+
+
+import chromedriver_binary  
+
+
+# In[14]:
 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import json
 import requests
+import json
 from time import sleep
 browser = webdriver.Chrome()
 browser.get('https://coinmarketcap.com/ja/currencies/bitcoin/')
@@ -15,19 +33,19 @@ coin_price=browser.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/di
 #ニュース一覧を上から順に繰り返します/body/div[1]/div/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div/span")
 
 
-# In[7]:
+# In[15]:
 
 
 coin_text="ビットコイン(BTC) "
 
 
-# In[8]:
+# In[16]:
 
 
 coin_text=coin_text+coin_price.text
 
 
-# In[39]:
+# In[10]:
 
 
 WEBHOOK_URL = "https://discordapp.com/api/webhooks/1003301610069426296/T4AoR7IJlspRulYO4ZuXw8_Ke2vVz68jDjajF99UUTeZ4veQfSsaPH0EPCMEGNhl3gjg?wait=true"
@@ -43,10 +61,10 @@ print( res.status_code )
 print( json.dumps( json.loads(res.content), indent=4, ensure_ascii=False ) )
 
 
-# In[41]:
+# In[21]:
 
 
-get_ipython().run_line_magic('run', 'C:\\Users\\jun19\\Downloads/bitcoin.py')
+
 
 
 # In[ ]:
